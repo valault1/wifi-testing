3bb6dd29e0edabba510b52f8426e377905e3af6f
Val Ault <val@Vals-MacBook-Air.local>
2026-03-31 15:51:32 -0700

    deleting extra stuff

--- _old/speedtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Log the date and the speed test results to a file
-echo "--- $(date) ---" >> ~/router_benchmark.log
-npx speed-cloudflare-cli >> ~/router_benchmark.log
-echo "" >> ~/router_benchmark.log
-
